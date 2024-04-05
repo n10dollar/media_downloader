cd4d2e6bb85eab4239479c65e86cf78951290029
Neil <ntendolkar@berkeley.edu>
2024-04-05 01:54:13 -0700

    Attempted conversion using ffmpeg

--- yt-to-mp3.py
+++ yt-to-mp3.py
@@ -1,33 +1,37 @@
 from youtubesearchpython import VideosSearch
 from yt_dlp import YoutubeDL
-import os
+from ffmpeg import stream
 
-def search_and_download(query):
-    # Search for videos matching the query
+def download_yt_video(query):
+    # search for videos matching the query
     videos_search = VideosSearch(query, limit=1)
     result = videos_search.result()
 
-    if not result['result']:
-        print("No video found matching the query.")
-        return
-
-    # Get the first video's ID and title
+    # get the first video's ID and title
     video_id = result['result'][0]['id']
     video_URL = result['result'][0]['link']
     video_title = result['result'][0]['title']
 
+    # configure parameters
     ydl_opts = {
         # 'format': 'bestaudio'
         # 'outtmpl': f'{video_title}.mp3',
     }
 
-    # Download the video and convert it to MP3
+    # download the video
     ydl = YoutubeDL(ydl_opts)
     ydl.download([video_URL])
+
+    file_name = video_title
+
+    # convert it to mp3
+    str = stream.Stream()
+    str.input(file_name)
+    str.out()
     
 
     print(f"Downloaded '{video_title}.mp3'")
 
 if __name__ == "__main__":
     query = input("Enter the title of the YouTube video: ")
-    search_and_download(query)
+    download_yt_video(query)
